981cfe3f129b11d67f512631f30ec34fe229a1e9
Asik1985 <assylbekezhanuly@yahoo.com>
2023-03-10 19:58:52 +0600

    init

---
+++ Shop/Models/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace Shop.Models
+{
+    public class Category
+    {
+        public int Category_id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Shop/Models/ShopContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Shop.Models
+{
+    public class ShopContext : DbContext
+    {
+
+public ShopContext(DbContextOptions<ShopContext> options) : base(options) 
+        {
+        
+        }
+        public DbSet<Product> Products { get; set;}
+        public DbSet<Category> Categories { get; set;}
+        public DbSet<Brand> Brands { get; set;}
+
+
+    }
+}
